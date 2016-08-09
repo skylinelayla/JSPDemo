5e15cf278a1be5d135ebb12b220fac3393f82ca6
SHIKUN <skylinelayla2012@gmail.com>
2016-08-09 11:18:03 +0800

    js test

--- web/js/Test.js
+++ web/js/Test.js
@@ -1,16 +1,19 @@
 /**
  * Created by SHIKUN on 2016/8/6.
- */
-var square = function (x) {
-    return x * x
-};
-var o = {a: 1, b: {c: 1}};
-var q = o.a;
-var d = o.b.c;
-
-var o2 = Object.create(null);
-var o3 = Object.create(Object.prototype);
-o = {x: 1, y: {z: [false, null, ""]}};
-s = JSON.stringify(o);
-
-window.location("http://www.baidu.com");
+// */
+//var square = function (x) {
+//    return x * x
+//};
+//var o = {a: 1, b: {c: 1}};
+//var q = o.a;
+//var d = o.b.c;
+//
+//var o2 = Object.create(null);
+//var o3 = Object.create(Object.prototype);
+//o = {x: 1, y: {z: [false, null, ""]}};
+//s = JSON.stringify(o);
+//
+//window.location("http://www.baidu.com");
+setTimeout(function () {
+    alert("hello world;")
+}, 2000);
